9f1b768d0ac77885d65d503e072763789934d405
idefant <inik3004@gmail.com>
2022-07-05 20:43:12 +0500

    upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 reportWebVitals();
