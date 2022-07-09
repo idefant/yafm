fb5da87f40f28cf4bcc264fcf1014a8567753a65
idefant <inik3004@gmail.com>
2022-07-09 12:21:09 +0500

    add redux-persist

--- src/schema/accountSchema.ts
+++ src/schema/accountSchema.ts
@@ -1,4 +1,4 @@
-import { object, string, number, boolean } from "yup";
+import { object, string, boolean } from "yup";
 
 export const accountSchema = object().shape({
   id: string().required(),
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import appReducer from "./reducers/appSlice";
 import categoryReducer from "./reducers/categorySlice";
@@ -8,10 +10,16 @@ import accountReducer from "./reducers/accountSlice";
 import transactionReducer from "./reducers/transactionSlice";
 import userReducer from "./reducers/userSlice";
 
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["api"],
+};
+
 export const rootReducer = combineReducers({
   app: appReducer,
   currency: currencyReducer,
-  user: userReducer,
+  user: persistReducer(persistConfig, userReducer),
   account: accountReducer,
   transaction: transactionReducer,
   category: categoryReducer,
@@ -20,6 +28,8 @@ export const rootReducer = combineReducers({
 export const store = configureStore({
   reducer: rootReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<() => typeof store>;
