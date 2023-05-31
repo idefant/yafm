bf3affb8b7bea95cefb2163df71ff21ee2e13afc
idefant <inik3004@gmail.com>
2023-05-31 15:03:45 +0500

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -39,27 +39,28 @@ const App: FC = () => {
         </>
       )}
 
-      {user && !password ? (
-        <>
-          <Route element={<CabinetTemplate />}>
-            <Route path="/decrypt/last" element={<Decrypt />} />
-            <Route path="/decrypt/:versionId" element={<Decrypt />} />
-            <Route path="/versions" element={<Versions />} />
-            <Route path="/upload" element={<Upload />} />
+      {user &&
+        (!password ? (
+          <>
+            <Route element={<CabinetTemplate />}>
+              <Route path="/decrypt/last" element={<Decrypt />} />
+              <Route path="/decrypt/:versionId" element={<Decrypt />} />
+              <Route path="/versions" element={<Versions />} />
+              <Route path="/upload" element={<Upload />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/decrypt/last" />} />
+          </>
+        ) : (
+          <Route element={<BaseTemplate />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/accounts" element={<Accounts />} />
+            <Route path="/setting" element={<Setting />} />
+            <Route path="templates" element={<Templates />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
-          <Route path="*" element={<Navigate to="/decrypt/last" />} />
-        </>
-      ) : (
-        <Route element={<BaseTemplate />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/setting" element={<Setting />} />
-          <Route path="templates" element={<Templates />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      )}
+        ))}
     </Routes>
   );
 };
