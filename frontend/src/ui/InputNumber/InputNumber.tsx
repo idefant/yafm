6c3252f9ee16b7ed1dbf11ff7f18a8295fa19313
idefant <inik3004@gmail.com>
2025-02-04 13:20:57 +0500

    add InputNumberBase & InputNumber components

---
+++ frontend/src/ui/InputNumber/InputNumber.tsx
@@ -0,0 +1,31 @@
+import { forwardRef, useRef } from 'react';
+import { mergeRefs } from 'react-merge-refs';
+import { Except } from 'type-fest';
+
+import { ControlExtraProps, InputControl } from '#ui/InputControl';
+import { InputNumberBase, InputNumberBaseProps } from '#ui/InputNumberBase';
+
+import { InputNumberClasses } from './inputNumberType';
+
+interface InputNumberProps extends Except<InputNumberBaseProps, 'classes'>, ControlExtraProps {
+  classes?: InputNumberClasses;
+}
+
+export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
+  ({ label, helper, classes, ...props }, ref) => {
+    const localRef = useRef<HTMLInputElement>();
+
+    return (
+      <InputControl
+        label={label}
+        inputRef={localRef}
+        required={props.required}
+        error={props.error}
+        helper={helper}
+        classes={classes}
+      >
+        <InputNumberBase ref={mergeRefs([ref, localRef])} {...props} />
+      </InputControl>
+    );
+  },
+);
