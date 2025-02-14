8e260c7a76b925d3f4c153f347d548854b7dd431
idefant <inik3004@gmail.com>
2025-02-15 01:13:42 +0500

    add Alert component

--- frontend/src/ui/Stack/VStack/VStack.tsx
+++ frontend/src/ui/Stack/VStack/VStack.tsx
@@ -3,7 +3,7 @@ import { Except } from 'type-fest';
 
 import { Flex, FlexProps } from '../Flex';
 
-interface VStackProps extends Except<FlexProps, 'direction'> {}
+export interface VStackProps extends Except<FlexProps, 'direction'> {}
 
 export const VStack: FC<VStackProps> = ({ gap = 8, ...props }) => (
   <Flex direction="column" align="stretch" gap={gap} {...props} />
